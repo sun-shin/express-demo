933381a54ccaaea31827881befacae78325cc4af
sun-shin <sunhanshin2@gmail.com>
2020-12-17 20:59:01 -0600

    update post and put actions

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Joi = require("joi");
 const express = require("express");
 const app = express();
-
+// joi.validate() no longer a fxn since v.16
 app.use(express.json());
 
 const courses = [
@@ -15,6 +15,36 @@ app.get("/", (req, res) => {
 app.get("/api/courses", (req, res) => {
   res.send(courses);
 });
+
+// app.post("/api/courses", (req, res) => {
+//   const course = {
+//     id: courses.length + 1,
+//     name: req.body.name,
+//   };
+//   courses.push(course);
+//   res.send(course);
+// });
+
+// app.post("/api/courses", (req, res) => {
+//   const schema = Joi.object({
+//     name: Joi.string().min(3).required(),
+//   });
+//   const validation = schema.validate(req.body);
+//   res.send(validation);
+
+//   if (!req.body.name || req.body.name.length < 3) {
+//     res
+//       .status(400)
+//       .send("Name is required and should be minimum of 3 characters");
+//     return;
+//   }
+//   const course = {
+//     id: courses.length + 1,
+//     name: req.body.name,
+//   };
+//   courses.push(course);
+//   res.send(course);
+// });
 
 app.post("/api/courses", (req, res) => {
   const { error } = validateCourse(req.body); //result.error
@@ -41,22 +71,22 @@ app.put("/api/courses/:id", (req, res) => {
 });
 
 function validateCourse(course) {
-  const schema = {
+  const schema = Joi.object({
     name: Joi.string().min(3).required(),
-  };
+  });
 
-  return Joi.validate(course, schema);
+  return schema.validate(course);
 }
 
-// app.delete("/api/courses/:id", (req, res) => {
-//   const course = courses.find((c) => c.id === parseInt(req.params.id));
-//   if (!course)
-//     return res.status(404).send("The course with the given ID was not found");
+app.delete("/api/courses/:id", (req, res) => {
+  const course = courses.find((c) => c.id === parseInt(req.params.id));
+  if (!course)
+    return res.status(404).send("The course with the given ID was not found");
 
-//   const index = courses.indexOf(course);
-//   courses.splice(index, 1);
-//   res.send(course);
-// });
+  const index = courses.indexOf(course);
+  courses.splice(index, 1);
+  res.send(course);
+});
 
 // PORT
 const port = process.env.PORT || 3000;
